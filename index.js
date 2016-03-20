962c2165ca6125e3a94e73e69d2e436648eb761e
Nelson Murillo Salazar <nel.mu.sa@gmail.com>
2016-03-20 06:00:09 -0600

    Cambios

--- index.js
+++ index.js
@@ -32,7 +32,8 @@ var app = {
         // if (device.platform == 'android' || device.platform == 'Android') {
             alert("Register called");
             //tu Project ID aca!!
-            pushNotification.register(this.successHandler, this.errorHandler,{"wai-global-001":"826441079868","ecb":"app.onNotificationGCM"});
+            // pushNotification.register(this.successHandler, this.errorHandler,{"senderID":"PROJECT_ID","ecb":"app.onNotificationGCM"});
+            pushNotification.register(this.successHandler, this.errorHandler,{"senderID":"826441079868","ecb":"app.onNotificationGCM"});
         // }
         // else {
             // alert("Register called");
