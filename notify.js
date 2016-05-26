44b31bca9912b10f9953833340abc514417092d7
Nelson Murillo Salazar <nel.mu.sa@gmail.com>
2016-05-26 01:14:15 -0600

    Cambios

--- notify.js
+++ notify.js
@@ -1,73 +1,69 @@
-function initialize() {
-    alert("inicia");
-    bindEvents();
-}
+var app = {
+// Application Constructor
+initialize: function() {
+	this.bindEvents();
+},
+// Bind Event Listeners
+//
+// Bind any events that are required on startup. Common events are:
+// 'load', 'deviceready', 'offline', and 'online'.
+bindEvents: function() {
+	document.addEventListener('deviceready', this.onDeviceReady, false);
+},
+// deviceready Event Handler
+//
+// The scope of 'this' is the event. In order to call the 'receivedEvent'
+// function, we must explicity call 'app.receivedEvent(...);'
+onDeviceReady: function() {
+	alert("The device is ready to use");
+	app.receivedEvent('deviceready');
+	alert("Sending project id to GCM server to register with the gcm");
+	var pushNotification = window.plugins.pushNotification;
+	pushNotification.register(app.successHandler, app.errorHandler,{"senderID":"826441079868","ecb":"app.onNotificationGCM"});
+},
+// Update DOM on a Received Event
+receivedEvent: function(id) {
+	alert("Entered in the received event");
+	var parentElement = document.getElementById(id);
+	var listeningElement = parentElement.querySelector('.listening');
+	var receivedElement = parentElement.querySelector('.received');
 
-function bindEvents() {
-    alert("add event listener");
-    document.addEventListener('deviceready', onDeviceReady, false);
-}
+	listeningElement.setAttribute('style', 'display:none;');
+	receivedElement.setAttribute('style', 'display:block;');
 
-function onDeviceReady() {
-    alert("device ready");
-    var pushNotification = window.plugins.PushNotification;
-    alert(device.platform);
-    if (device.platform == 'android') {
-        alert("Register called " + device.platform);
-        // pushNotification.register(this.successHandler, this.errorHandler,{"senderID":"PROJECT_ID","ecb":"app.onNotificationGCM"});
-        pushNotification.register(this.successHandler, this.errorHandler,{"senderID":"826441079868","ecb":"app.onNotificationGCM"});
-    } else {
-        alert("Register called " + device.platform);
-        pushNotification.register(this.successHandler,this.errorHandler,{"badge":"true","sound":"true","alert":"true","ecb":"app.onNotificationAPN"});
-    }
-}    
-
+	console.log('Received Event: ' + id);
+},
 // result contains any message sent from the plugin call
-function successHandler(result) {
-    alert('Callback Success! Result = ' + result);
+successHandler: function(result) {
+	alert('Callback Success! Result = '+result)
+},
+errorHandler:function(error) {
+	alert(error);
+},
+onNotificationGCM: function(e) {
+	alert("In the onNotificationGCM " + e.event);
+	switch( e.event )
+	{
+		case 'registered':
+		if ( e.regid.length > 0 )
+		{
+			console.log("Regid " + e.regid);
+			alert('registration id = '+e.regid);
+		}
+		break;
+
+		case 'message':
+		alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+		break;
+
+		case 'error':
+		alert('GCM error = '+e.msg);
+		break;
+
+		default:
+		alert('An unknown GCM event has occurred');
+		break;
+	}
 }
 
-function errorHandler(error) {
-    alert(error);
-}
-
-function onNotificationGCM(e) {
-    switch( e.event ) {
-        case 'registered':
-        if ( e.regid.length > 0 ) {
-            alert('registration id = ' + e.regid);
-        }
-        break;
-
-        case 'message':
-        alert('message = '+e.message+' msgcnt = '+e.msgcnt);
-        break;
-
-        case 'error':
-        alert('GCM error = '+e.msg);
-        break;
-
-        default:
-        alert('An unknown GCM event has occurred');
-        break;
-    }
-}
-
-// <form id="formulario" name="formulario" method="post" action="http://www.wai-news.com/php/notify.php">
-
-
-function onNotificationAPN(event) {
-    var pushNotification = window.plugins.pushNotification;
-    alert("Running in JS - onNotificationAPN - Received a notification! " + event.alert);
-
-    if (event.alert) {
-        navigator.notification.alert(event.alert);
-    }
-    if (event.badge) {
-        pushNotification.setApplicationIconBadgeNumber(this.successHandler, this.errorHandler, event.badge);
-    }
-    if (event.sound) {
-        var snd = new Media(event.sound);
-        snd.play();
-    }
-}
+}; 
