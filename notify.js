911550e128b4bdbc5d920dbfd71a3fcc92a3f31e
Nelson Murillo Salazar <nel.mu.sa@gmail.com>
2016-05-25 23:09:07 -0600

    Cambios

--- notify.js
+++ notify.js
@@ -1,84 +1,62 @@
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
+function initialize() {
+    bindEvents();
+}
+
+function bindEvents() {
+    document.addEventListener('deviceready', onDeviceReady, false);
+}
+
+function onDeviceReady() {
+    receivedEvent('deviceready');
+}
     
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-        var pushNotification = window.plugins.pushNotification;
-        // if (device.platform == 'android' || device.platform == 'Android') {
-            alert("Register called");
-            //tu Project ID aca!!
-            // pushNotification.register(this.successHandler, this.errorHandler,{"senderID":"PROJECT_ID","ecb":"app.onNotificationGCM"});
-            pushNotification.register(this.successHandler, this.errorHandler,{"senderID":"826441079868","ecb":"app.onNotificationGCM"});
-        // }
-        // else {
-            // alert("Register called");
-            // pushNotification.register(this.successHandler,this.errorHandler,{"badge":"true","sound":"true","alert":"true","ecb":"app.onNotificationAPN"});
-        // }
-    },
+function receivedEvent(id) {
+    var pushNotification = window.plugins.pushNotification;
+    if (device.platform == 'android' || device.platform == 'Android') {
+        alert("Register called " + device.platform);
+        // pushNotification.register(this.successHandler, this.errorHandler,{"senderID":"PROJECT_ID","ecb":"app.onNotificationGCM"});
+        pushNotification.register(this.successHandler, this.errorHandler,{"senderID":"826441079868","ecb":"app.onNotificationGCM"});
+    } else {
+        alert("Register called " + device.platform);
+        pushNotification.register(this.successHandler,this.errorHandler,{"badge":"true","sound":"true","alert":"true","ecb":"app.onNotificationAPN"});
+    }
+    
     // result contains any message sent from the plugin call
-    successHandler: function(result) {
-        alert('Callback Success! Result = '+result)
-    },
-    errorHandler:function(error) {
+    function successHandler(result) {
+        alert('Callback Success! Result = ' + result);
+    }
+
+    function errorHandler(error) {
         alert(error);
-    },
-    onNotificationGCM: function(e) {
-        switch( e.event )
-        {
+    }
+
+    function onNotificationGCM(e) {
+        switch( e.event ) {
             case 'registered':
-            if ( e.regid.length > 0 )
-            {
-                console.log("Regid " + e.regid);
-                alert('registration id = '+e.regid);
-                    //Cuando se registre le pasamos el regid al input
-                    document.getElementById('regId').value = e.regid;
+                if ( e.regid.length > 0 ) {
+                    alert('registration id = ' + e.regid);
                 }
                 break;
 
-                case 'message':
+            case 'message':
               // NOTIFICACION!!!
-              alert('message = '+e.message+' msgcnt = '+e.msgcnt);
-              break;
+                alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+                break;
 
-              case 'error':
-              alert('GCM error = '+e.msg);
-              break;
+            case 'error':
+                alert('GCM error = '+e.msg);
+                break;
 
-              default:
-              alert('An unknown GCM event has occurred');
-              break;
-          }
-      },
+            default:
+                alert('An unknown GCM event has occurred');
+                break;
+        }
+    }
 
 // <form id="formulario" name="formulario" method="post" action="http://www.wai-news.com/php/notify.php">
 
       
-      onNotificationAPN: function(event) {
+    function onNotificationAPN(event) {
         var pushNotification = window.plugins.pushNotification;
         alert("Running in JS - onNotificationAPN - Received a notification! " + event.alert);
         
@@ -93,5 +71,3 @@ var app = {
             snd.play();
         }
     }
-};
-
