d4ed224311d55bf6ac36bdb53ff1477caed77084
Nelson Murillo Salazar <nel.mu.sa@gmail.com>
2016-06-11 10:26:28 -0600

    Cambios

---
+++ js/notify_ios_old.js
@@ -0,0 +1,63 @@
+var a = document.getElementById("a");
+var b = document.getElementById("b");
+var c = document.getElementById("c");
+var d = document.getElementById("d");
+var e = document.getElementById("e");
+var f = document.getElementById("f");
+
+function prueba() {
+	c.style.display = "block";
+	var uurrll = "http://www.wai-news.com/index.php?option=com_jbackend&view=request&action=put&module=push&resource=register&token=APA91bF-O4Wl0lSZO0GXDBvzb27T6H3KSmkXqCcFzbnYwORc0t0wmjHjoy4wqaY9DgUO8XIxW5OwSGofvNm6ma79D0CkXTRZMbOf0NifMVJQ01gsB_3KHsx-leQMdEC5JWjdSeArNzyi&appcode=wai-notify-001-no&platform=ios";
+	var myWindow = window.open(uurrll, '_blank', 'location=no');
+    setTimeout(function(){ myWindow.close() }, 1000);
+}
+
+function initialize() {
+	a.style.display = "block";
+	this.bindEvents();
+}
+
+function bindEvents() {
+	b.style.display = "block";
+	document.addEventListener("deviceready", onDeviceReady, false);
+	// document.addEventListener("deviceready", prueba, false);
+}
+
+function onDeviceReady() {
+	c.style.display = "block";
+	var pushNotification = window.plugins.pushNotification;
+// if (device.platform == 'android') {
+	// pushNotification.register(successHandler, errorHandler,{"senderID":"508191947380","ecb":"onNotificationGCM"});
+// } else {
+	pushNotification.register(successHandler, errorHandler,{"badge":"true","sound":"true","alert":"true","ecb":"onNotificationAPN"});
+// }
+}
+
+function successHandler(result) {
+	if (result = "ok") {
+		d.style.display = "block";
+	}
+// alert('Callback Success! Result = '+result);
+}
+
+function errorHandler(error) {
+	e.style.display = "block";
+	alert('Error: '+ error);
+}
+
+function onNotificationAPN(event) {
+	alert("Running in JS - onNotificationAPN - Received a notification! " + event.alert);
+	f.style.display = "block";
+
+	if (event.alert) {
+		navigator.notification.alert(event.alert);
+	}
+	if (event.badge) {
+		pushNotification.setApplicationIconBadgeNumber(successHandler, errorHandler, event.badge);
+	}
+	if (event.sound) {
+		var snd = new Media(event.sound);
+		snd.play();
+	}
+
+}
