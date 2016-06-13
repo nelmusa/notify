56fb20f93e5a8150445ab32f2a21db91723afd70
Nelson Murillo Salazar <nel.mu.sa@gmail.com>
2016-06-13 13:04:06 -0600

    Cambios

--- js/notify_android.js
+++ js/notify_android.js
@@ -1,81 +1,65 @@
-var a = document.getElementById("a");
-var b = document.getElementById("b");
-var c = document.getElementById("c");
-var d = document.getElementById("d");
-var e = document.getElementById("e");
-var f = document.getElementById("f");
-
-function prueba() {
-	c.style.display = "block";
-	var uurrll = "http://www.wai-news.com/index.php?option=com_jbackend&view=request&action=put&module=push&resource=register&token=APA91bF-O4Wl0lSZO0GXDBvzb27T6H3KSmkXqCcFzbnYwORc0t0wmjHjoy4wqaY9DgUO8XIxW5OwSGofvNm6ma79D0CkXTRZMbOf0NifMVJQ01gsB_3KHsx-leQMdEC5JWjdSeArNzyi&appcode=wai-notify-001-no&platform=android";
-	var myWindow = window.open(uurrll, '_blank', 'location=no');
-    setTimeout(function(){ myWindow.close() }, 1000);
-}
-
-function initialize() {
-	// var iidd = localStorage.idnotify;
-	// if (iidd == "") {
+var app = {
+	initialize: function() {
 		a.style.display = "block";
 		this.bindEvents();
-	// } else {
-		// f.style.display = "block";
-	// }
-}
 
-function bindEvents() {
-	b.style.display = "block";
-	document.addEventListener("deviceready", onDeviceReady, false);
-	// document.addEventListener("deviceready", prueba, false);
-}
+	},
+	bindEvents: function() {
+		b.style.display = "block";
+		document.addEventListener('deviceready', this.onDeviceReady, false);
+	},
+	onDeviceReady: function() {
+		c.style.display = "block";
+		app.receivedEvent('deviceready');
+	},
+	receivedEvent: function(id) {
+		// var parentElement = document.getElementById(id);
+		// var listeningElement = parentElement.querySelector('.listening');
+		// var receivedElement = parentElement.querySelector('.received');
 
-function onDeviceReady() {
-	c.style.display = "block";
-	var pushNotification = window.plugins.pushNotification;
-// if (device.platform == 'android') {
-	pushNotification.register(successHandler, errorHandler,{"senderID":"508191947380","ecb":"onNotificationGCM"});
-// } else {
-	// pushNotification.register(successHandler, errorHandler,{"badge":"true","sound":"true","alert":"true","ecb":"onNotificationAPN"});
-// }
-}
-
-function successHandler(result) {
-	if (result = "ok") {
-		d.style.display = "block";
-	}
-// alert('Callback Success! Result = '+result);
-}
-
-function errorHandler(error) {
-	e.style.display = "block";
-	alert('Error: '+ error);
-}
-
-function onNotificationGCM(e) {
-	switch( e.event ) {
-		case 'registered':
-		if ( e.regid.length > 0 ) {
-			var uurrll = "http://www.wai-news.com/index.php?option=com_jbackend&view=request&action=put&module=push&resource=register&token=" + e.regid + "&appcode=wai-notify-001&platform=android";
-			alert(uurrll);
-			// var uurrll = "http://www.wai-news.com/index.php?option=com_jbackend&view=request&action=put&module=push&resource=register&token=" + e.regid + "&appcode=wai-notify-001&platform=ios";
-			var myWindow = window.open(uurrll, '_blank', 'location=no');
-			setTimeout(function(){ myWindow.close() }, 1000);
-
-			localStorage.idnotify = e.regid;
-			f.style.display = "block";
+		// listeningElement.setAttribute('style', 'display:none;');
+		// receivedElement.setAttribute('style', 'display:block;');
+		var pushNotification = window.plugins.pushNotification;
+		alert("Register called Android");
+		pushNotification.register(successHandler, errorHandler,{"senderID":"508191947380","ecb":"onNotificationGCM"});
+	},
+	successHandler: function(result) {
+		if (result = "ok") {
+			d.style.display = "block";
 		}
-		alert('Registration id: ' + e.regid);
-		break;
+	},
+	errorHandler:function(error) {
+		e.style.display = "block";
+		alert('Error: '+ error);
+	},
+	onNotificationGCM: function(e) {
+		switch( e.event ) {
+			case 'registered':
+			if ( e.regid.length > 0 ) {
+				var uurrll = "http://www.wai-news.com/index.php?option=com_jbackend&view=request&action=put&module=push&resource=register&token=" + e.regid + "&appcode=wai-notify-001&platform=android";
+				alert(uurrll);
+				// var uurrll = "http://www.wai-news.com/index.php?option=com_jbackend&view=request&action=put&module=push&resource=register&token=" + e.regid + "&appcode=wai-notify-001&platform=ios";
+				var myWindow = window.open(uurrll, '_blank', 'location=no');
+				setTimeout(function(){ myWindow.close() }, 1000);
 
-		case 'message':
-		alert('message = '+e.message+' msgcnt = '+e.msgcnt);
-		break;
+				localStorage.idnotify = e.regid;
+				f.style.display = "block";
+			}
+			alert('Registration id: ' + e.regid);
+			break;
 
-		case 'error':
-		alert('GCM error = '+e.msg);
-		break;
+			case 'message':
+			alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+			break;
 
-		default:
-		alert('An unknown GCM event has occurred');
-		break;
+			case 'error':
+			alert('GCM error = '+e.msg);
+			break;
+
+			default:
+			alert('An unknown GCM event has occurred');
+			break;
+		}
 	}
-}
+};
+
